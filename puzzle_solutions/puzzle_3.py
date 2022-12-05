f60497810547a211d2945838fb198673e59d7076
Daniel Jones DOS <lis4dyslexia@gmail.com>
2022-12-05 16:58:32 +0000

    Puzzle 3: Sets are cool Puzzle 4: use dataclasses if you hate to __init__()

---
+++ puzzle_solutions/puzzle_3.py
@@ -0,0 +1,48 @@
+# ? COPY ME FOR EACH PUZZLE
+import string
+from my_utils.reader import get_puzzle_input
+from my_utils.logger import get_logger
+
+INPUT_FILE = __file__.split(".")[0].replace("puzzle_solutions", "puzzle_input") + ".txt"
+INPUT = get_puzzle_input(INPUT_FILE)
+logger = get_logger()
+
+def get_priority(character: str) -> int:
+    if character in string.ascii_lowercase:
+        return ord(character) - 96
+    if character in string.ascii_uppercase:
+        return ord(character) - 38
+    return 0
+        
+
+def part_1() -> int:
+    letters: list[str] = []
+    for line in INPUT:
+        midpoint = len(line) // 2
+        one, two = line[:midpoint], line[midpoint:]
+        for letter in one:
+            if letter in two:
+                letters.append(letter)
+                break
+    result = 0
+    for letter in letters:
+        result += get_priority(letter)
+    
+    return result
+
+def part_2():
+    badges: list[str] = []
+    for i in range(0, len(INPUT), 3):
+        intersection = set(INPUT[i]).intersection(INPUT[i+1]).intersection(INPUT[i+2])
+        badges.append(intersection.pop())
+    result = sum([get_priority(badge) for badge in badges])
+    return result
+        
+
+
+def main() -> None:
+    print(f"The answer to part 1 is {part_1()}")
+    print(f"The answer to part 2 is {part_2()}")
+
+if __name__ == "__main__":
+    main()
---
+++ puzzle_solutions/puzzle_4.py
@@ -0,0 +1,59 @@
+# ? COPY ME FOR EACH PUZZLE
+from dataclasses import dataclass
+from my_utils.reader import get_puzzle_input
+from my_utils.logger import get_logger
+
+INPUT_FILE = __file__.split(".")[0].replace("puzzle_solutions", "puzzle_input") + ".txt"
+INPUT = get_puzzle_input(INPUT_FILE)
+logger = get_logger()
+
+
+@dataclass
+class Elf:
+    start: int
+    end: int
+
+    def check_containment(self, other: "Elf") -> bool:
+        if self.start >= other.start and self.end <= other.end:
+            return True
+        if self.start <= other.start and self.end >= other.end:
+            return True
+        return False
+
+    def check_overlap(self, other: "Elf") -> bool:
+        if self.start > other.end:
+            return False
+        if self.end < other.start:
+            return False
+        return True
+
+
+def part_1():
+    contains = 0
+    for line in INPUT:
+        elf_1_str, elf_2_str = line.split(",")[0], line.split(",")[1]
+        elf_1: Elf = Elf(int(elf_1_str.split("-")[0]), int(elf_1_str.split("-")[1]))
+        elf_2: Elf = Elf(int(elf_2_str.split("-")[0]), int(elf_2_str.split("-")[1]))
+        if elf_1.check_containment(elf_2):
+            contains += 1
+    return contains
+
+
+def part_2():
+    overlaps = 0
+    for line in INPUT:
+        elf_1_str, elf_2_str = line.split(",")[0], line.split(",")[1]
+        elf_1: Elf = Elf(int(elf_1_str.split("-")[0]), int(elf_1_str.split("-")[1]))
+        elf_2: Elf = Elf(int(elf_2_str.split("-")[0]), int(elf_2_str.split("-")[1]))
+        if elf_1.check_overlap(elf_2):
+            overlaps += 1
+    return overlaps
+
+
+def main() -> None:
+    print(f"The answer to part 1 is {part_1()}")
+    print(f"The answer to part 2 is {part_2()}")
+
+
+if __name__ == "__main__":
+    main()
